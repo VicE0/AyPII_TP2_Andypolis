d087a562c5762e602313c3faa3f6e943f7f4d85d
VicE0 <vespeche@fi.uba.ar>
2021-10-31 00:45:52 -0300

    Añado la clase de ubicaciones (no terminado, no funciona)

---
+++ calse_ubicaciones/Ubicaciones.h
@@ -0,0 +1,20 @@
+#ifndef UBICACIONES_H
+#define UBICACIONES_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Ubicacion
+{
+    public:
+        string nombre;
+        char parentesis_abierto;
+        char parentesis_cerrado;
+
+};
+
+
+
+#endif // UBICACIONES_H
